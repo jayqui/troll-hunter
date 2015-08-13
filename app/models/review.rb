da6a01cc8371c6f76e486330777cf5340f8d8da6
Andrew Carr <aacarr5@gmail.com>
2015-08-13 13:56:59 -0500

    Adds worthy validation

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,17 +2,23 @@ class Review < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :restaurant
 
+  validate :worthy?
+
   SEXY_WORDS = %(sexy sex sexual voluptuous penis masturbation masturbate erotic come-hither sensuous suggestive titillating seductive racy inviting provacative)
 
   DRAMATIC_WORDS = %w(breathtaking vivid tragic emotional climatic farcical dramatic histrionic tragicomic striking powerful sensational startling thrilling powerful profound)
 
+  def worthy?
+      sexual? || dramatic?
+      # errors.add(:worthy,"Sorry. Not worthy enough?")
+  end
 
   def self.too_sexual
     Review.all.select {|review| review.sexual?}
   end
 
   def sexual?
-    total_words = this.body.split(" ")
+    total_words = self.body.split(" ")
     total_words.any?{|word| SEXY_WORDS.include?(word)}
   end
 
