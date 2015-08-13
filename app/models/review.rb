6f4683a9e0c0c5238cccf1dcf16da75d3739bdc2
Andrew Carr <aacarr5@gmail.com>
2015-08-13 12:49:12 -0500

    Adds too sexy and too dramatic methods

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,29 @@
 class Review < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :restaurant
+
+  SEXY_WORDS = %(sexy sex sexual voluptuous penis masturbation masturbate erotic come-hither sensuous suggestive titillating seductive racy inviting provacative)
+
+  DRAMATIC_WORDS = %w(breathtaking vivid tragic emotional climatic farcical histrionic tragicomic striking powerful sensational startling thrilling powerful profound)
+
+
+  def Self.too_sexual
+    Review.all.select {|review| review.sexual?}
+  end
+
+  def sexual?
+    total_words = this.body.split(" ")
+    total_words.any?{|word| SEXY_WORDS.include?(word)}
+  end
+
+  def Self.too_dramatic
+    Review.all.select{|review| review.dramatic?}
+  end
+
+  def dramatic?
+    total_words = this.body.split(" ")
+    total_words.any?{|word| DRAMATIC_WORDS.include?(word)}
+  end
+
+
 end
