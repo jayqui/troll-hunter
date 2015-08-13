2543c00944dbee0bba75461eefad1bb458b75286
Jay Quigley <quigley.james.g@gmail.com>
2015-08-13 10:15:21 -0500

    add migration details

--- db/migrate/20150813100636_create_reviews.rb
+++ db/migrate/20150813100636_create_reviews.rb
@@ -1,4 +1,13 @@
 class CreateReviews < ActiveRecord::Migration
   def change
+  	create_table :reviews do |t|
+  		t.string :body
+  		t.string :title
+  		t.string :url
+  		t.integer :stars
+  		t.references :restaurant
+  		t.references :user
+  		t.timestamps
+  	end
   end
 end
--- db/migrate/20150813100645_create_restaurants.rb
+++ db/migrate/20150813100645_create_restaurants.rb
@@ -1,4 +1,10 @@
 class CreateRestaurants < ActiveRecord::Migration
   def change
+  	create_table :restaurants do |t|
+  		t.string :name
+  		t.integer :stars_average
+  		t.string :business_type
+  		t.timestamps
+  	end
   end
 end
