15b2ee0f00a6e2de569556226a19ab1183cae372
Andrew Carr <aacarr5@gmail.com>
2015-08-13 14:06:16 -0500

    add initial scraing and CSV file of reviews for Alinea

---
+++ db/parse/alinea_manipulation.rb
@@ -0,0 +1,16 @@
+require_relative 'nokogiri_test'
+require 'csv'
+
+reviews_array = @alinea.search(".review-content > p").map {|review| review.inner_text}
+reviews_array.uniq!
+p reviews_array.count
+
+CSV.open("db/parse/reviews_alinea.csv", "wb") do |csv|
+  reviews_array.each do |review|
+    csv << [review]
+  end
+end
+
+
+
+
