eb75a929ebffef49606739ecf118e699fa549c9a
Andrew Carr <aacarr5@gmail.com>
2015-08-13 18:43:42 -0500

    Add seed data for reviews and restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,13 @@
-# CSV.foreach("./db/parse/reviews_alinea.csv") do |review|
-#   Review.create(body:review,url:"www.yelp.com/biz/alinea-chicago")
-# end
+def create_seeds(file)
+  return if file == "." || file == ".."
+  restaurant = Restaurant.create(name: CSV.open("db/csv_files/#{file}",'r') {|csv| csv.first})
 
- SEXY_WORDS = %w(sexy sex sexual voluptuous penis masturbation masturbate erotic come-hither sensuous suggestive titillating seductive racy inviting provacative mistress dick orgy)
-
-  DRAMATIC_WORDS = %w(gross pathetic miserable tacky Kardashian jesus Jesus Christ ex-husband ex-wife ex-partner ex-boyfriend ex-girlfriend fuck damn shit cock bitch asshole asshat twerk terrible horrible 9/11 scum vile fecle)
-
-require 'faker'
-
-5.times do
-  restaurant = Restaurant.create(name:"The restaurant of #{Faker::Name.name}")
-
-  10.times do
-    Review.create(body:"This restaurant is #{SEXY_WORDS.sample}",restaurant:restaurant)
+  CSV.foreach("db/csv_files/#{file}",headers:true) do |review|
+    Review.create(body:review,restaurant_id:restaurant.id)
   end
-
 end
+
+
+
+Dir.foreach("db/csv_files") {|file| create_seeds(file) }
+
