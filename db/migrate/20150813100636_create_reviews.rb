303ce8c8e46bfc65f6f40cf2995910784c279689
Andrew Carr <aacarr5@gmail.com>
2015-08-13 13:57:32 -0500

    Drops columns from reviews database

--- db/migrate/20150813100636_create_reviews.rb
+++ db/migrate/20150813100636_create_reviews.rb
@@ -2,9 +2,7 @@ class CreateReviews < ActiveRecord::Migration
   def change
   	create_table :reviews do |t|
   		t.string :body
-  		t.string :title
   		t.string :url
-  		t.integer :stars
   		t.references :restaurant
   		t.references :user
   		t.timestamps
