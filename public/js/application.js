6a191b4c2d1a7df4a559fed67bcdeba91683b2d7
Andrew Carr <aacarr5@gmail.com>
2015-08-13 20:56:44 -0500

    Adds attemp1 javascript

---
+++ public/js/application.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+    var sexyWords = ["sexy", "sex", "sexual", "voluptuous", "penis", "vagina", "pussy", "masturbation", "masturbate", "erotic", "come-hither", "sensuous", "suggestive", "titillating", "seductive", "racy", "inviting", "provacative", "mistress", "dick", "orgy", "orgasm"]
+
+    var dramaticWords =["gross", "pathetic", "miserable", "tacky", "Kardashian", "jesus", "Jesus", "Christ", "God", "god", "ex-husband", "ex-wife", "ex-partner", "ex-boyfriend", "ex-girlfriend", "fuck", "fucking", "motherfucking", "motherfucker", "damn", "goddamn", "shit", "shitty", "crap", "crappy", "cock", "bitch", "cunt", "asshole", "asshat", "twerk", "terrible", "horrible", "9/11", "scum", "vile", "fecle", "fecal"]
+
+    text = $(".review-content").text().split(" ");
+
+    for (var i = 0; i < text.length;i++) {
+        if $.inArray(text[i],sexyWords) {
+            $(text[i]).addClass("flag-word")
+        }
+    }
+
+
+})
