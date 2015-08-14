d24fd355b7bb85688d5b451dbbf19e66960a176a
Andrew Carr <aacarr5@gmail.com>
2015-08-13 20:07:40 -0500

    Adds shows page with links

--- app/controllers/restaurants.rb
+++ app/controllers/restaurants.rb
@@ -2,3 +2,8 @@ get '/restaurants' do
   @restaurants = Restaurant.all.sort {|restaurant| restaurant.reviews.count}[0..9]
   erb :"restaurants/index"
 end
+
+get '/restaurants/:id' do
+  @restaurant = Restaurant.find(params[:id])
+  erb :"restaurants/show"
+end
