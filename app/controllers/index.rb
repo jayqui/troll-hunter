3367545286bc5e649fa6c7797073e0a89014f78b
Andrew Carr <aacarr5@gmail.com>
2015-08-13 19:19:08 -0500

    add index page view

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,3 +1,4 @@
 get '/' do
+  @restaurants = Restaurant.all.sort_by{|restaurant| restaurant.reviews.count}.reverse[0..9]
   erb :index
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,7 @@
 class Restaurant < ActiveRecord::Base
 	has_many  :reviews
+
+  def full_name
+    self.name[2..-3]
+  end
 end
