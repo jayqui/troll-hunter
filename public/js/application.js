6e7785e1ea76129f603370fb03ef94191aee46a2
Jay Quigley <email": null>
2015-08-14 15:00:12 -0500

    only pass in initial 10 reviews to restaurant show page

--- public/js/application.js
+++ public/js/application.js
@@ -1,16 +1,3 @@
 $(document).ready(function() {
 
-    var sexyWords = ["sexy", "sex", "sexual", "voluptuous", "penis", "vagina", "pussy", "masturbation", "masturbate", "erotic", "come-hither", "sensuous", "suggestive", "titillating", "seductive", "racy", "inviting", "provacative", "mistress", "dick", "orgy", "orgasm"]
-
-    var dramaticWords =["gross", "pathetic", "miserable", "tacky", "Kardashian", "jesus", "Jesus", "Christ", "God", "god", "ex-husband", "ex-wife", "ex-partner", "ex-boyfriend", "ex-girlfriend", "fuck", "fucking", "motherfucking", "motherfucker", "damn", "goddamn", "shit", "shitty", "crap", "crappy", "cock", "bitch", "cunt", "asshole", "asshat", "twerk", "terrible", "horrible", "9/11", "scum", "vile", "fecle", "fecal"]
-
-    // var text = $(".review-content").text().split(" ");
-
-    // for (var i = 0; i < text.length;i++) {
-    //     if $.inArray(text[i],sexyWords) {
-    //         $(text[i]).addClass("flag-word")
-    //     }
-    // }
-
-
 })
